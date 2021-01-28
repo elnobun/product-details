fbf967ac97b668f85f9c18df01e94ad8d7f74862
elnobun <elnobun@gmail.com>
2021-01-28 02:06:48 +0000

    updated component and styles

--- src/components/ProductDetails.vue
+++ src/components/ProductDetails.vue
@@ -1,24 +1,36 @@
 <template>
   <div class="product">
 
+    <!--  Product Title  -->
+    <div class="product__title">
+        <h1 class="align">{{ product.title }}</h1>
+    </div>
+
+    <!-- Contributor Author -->
+    <div class="product__author-name align" v-for="author in product.contributors" :key="author.name">
+        <p>By <span>{{ author.contributor.name }}</span></p>
+    </div>
+
+    <!--  Author Bio  -->
+    <div class="product__about-bio paper" v-for="author in product.contributors" :key="author.name">
+      <h4 class="header__caption">About The Author</h4>
+      <span v-html="author.contributor.bio"></span>
+    </div>
+
     <!--  Product Image  -->
     <div class="product__image">
       <img class="paper" :src="product.image" alt="book image">
     </div>
 
-    <!--  Product Title  -->
-    <div class="product__title">
-      <h1>{{ product.title }}</h1>
-      <!-- Contributor Name and Bio -->
-      <div class="product__author-name" v-for="author in product.contributors" :key="author.name">
-        <p>By <span>{{ author.contributor.name }}</span></p>
-      </div>
+    <!-- Product Description -->
+    <div class="product__description paper">
+      <h4 class="header__caption">Product Description</h4>
+      <span v-html="formatData(product.description, /&nbsp/g)"></span>
     </div>
 
-<!--    <hr class="line">-->
 
     <!--  Product Prices -->
-    <div class="product__prices">
+    <div class="product__prices align">
       <sub class="price_tag">Prices</sub>
       <div class="price__amount">
         <div v-for="(price, index) in product.prices" :key="index">
@@ -29,28 +41,17 @@
 
 
     <!-- Product Sale Date -->
-    <div class="product__sale">
+    <div class="product__sale align">
       <span class="sale__date">Sale Date:</span>
-      <span> March 15, {{ product.sale_date.date.replace(/-03-15 00:00:00.000000/, '') }}</span>
+      <span> March 15, {{ formatData(product.sale_date.date, /-03-15 00:00:00.000000/)}}</span>
     </div>
 
-    <!--  Author Bio  -->
-    <div class="product__about-bio paper" v-for="author in product.contributors" :key="author.name">
-      <h4 class="header__caption">About The Author</h4>
-      <span v-html="author.contributor.bio"></span>
-    </div>
-
-    <!-- Product Description -->
-    <div class="product__description paper">
-      <h4 class="header__caption">Product Description</h4>
-      <span v-html="product.description.replace(/&nbsp/g, '')"></span>
-    </div>
 
     <!-- Product Reviews -->
     <div class="product__reviews paper">
       <h4 class="header__caption">Reviews</h4>
       <div class="reviews" v-for="reviews in product.reviews" :key="reviews.review_id">
-        <q>{{ reviews.review.description.replace(/[“”]/g, '') }}</q>
+        <q>{{formatData(reviews.review.description, /[“”]/g )}}</q>
         <p v-html="reviews.review.reviewer"></p>
       </div>
     </div>
@@ -76,14 +77,25 @@ import { ref } from 'vue'
 export default {
   name: 'ProductDetails',
 
+  // VueJS3 composition API, doing asynchronous job under the hood.
   async setup () {
+    // Return product default state
     const product = ref(null)
 
+    // Fetch JSON data
     const productResponse = await fetch('https://v3-static.supadu.io/dev/products/9780060577315.json')
+    // Set product value to JSON response
     product.value = await productResponse.json()
 
+    // Function to format HTML data
+    function formatData(data, str) {
+      return data.replace(str, "")
+    }
+
+    // Return data object, so that it becomes available to use in the template
     return {
       product,
+      formatData
     }
   },
 }
